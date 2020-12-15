ba3decfedc81d19252fab905a2c968ca7130a3e5
Chris Berry <chris.berry07@gmail.com>
2020-12-14 22:03:23 -0600

    Major update
    Added build numbering. Added additional top-nav items. Centered vertically and horizontally 404 page text. Moved build numbering lower. Added bootstrap scripts for dropdowns. (may not need). Updated NPM scripts to run version number on build.

---
+++ generate-buildno.js
@@ -0,0 +1,18 @@
+/**
+ * Auto increment build number in a JavaScript app
+ * https://richhewlett.com/2020/07/11/auto-increment-build-number-in-a-javascript-app/
+ *
+ * **/
+
+
+const fs = require('fs');
+console.log('Incrementing build number...');
+fs.readFile('src/metadata.json',function(err,content) {
+    if (err) throw err;
+    const metadata = JSON.parse(content);
+    metadata.buildRevision = metadata.buildRevision + 1;
+    fs.writeFile('src/metadata.json',JSON.stringify(metadata),function(err){
+        if (err) throw err;
+        console.log(`Current build number: ${metadata.buildMajor}.${metadata.buildMinor}.${metadata.buildRevision} ${metadata.buildTag}`);
+    })
+});
