74b8d0b881bf966118fbb53b95452562bdec398e
Chris Berry <chris.berry07@gmail.com>
2021-01-04 13:55:24 -0600

    Update about-page.js

--- src/components/pages/about-page.js
+++ src/components/pages/about-page.js
@@ -5,6 +5,9 @@ import DefaultLayout from "../start-layout/default-layout"
 const AboutPage = () => (
   <DefaultLayout>
     <h1><i className="fa fa-info-circle"  aria-hidden="true"></i> About</h1>
+   <p>This application has been built using React. By utilizing React we have been able to create a simple
+      yet effective development cycle of creating common and specific components which are then used tobe unique
+      sections of the application. </p>
     <p>
       <Link to="/">Link to home page</Link>
     </p>
